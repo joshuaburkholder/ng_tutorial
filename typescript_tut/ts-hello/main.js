854de48f2d164a79545077b3d75588ef5aaf60df
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-10-04 23:04:42 -0400

    update

--- typescript_tut/ts-hello/main.js
+++ typescript_tut/ts-hello/main.js
@@ -1,5 +1,10 @@
-function log(message) {
-    console.log(message);
+//in typescript there are two ways to declare variables:
+var number = 1;
+var count = 2;
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('finally ' + i);
 }
-var message = 'hello world';
-log(message);
+doSomething();
--- typescript_tut/ts-hello/main.ts
+++ typescript_tut/ts-hello/main.ts
@@ -3,3 +3,13 @@
 
 var number = 1;
 let count = 2;
+
+function doSomething() {
+    for (let i=0; i < 5; i++){
+        console.log(i);
+    }
+    console.log ('finally ' + i)
+}
+
+doSomething();
+

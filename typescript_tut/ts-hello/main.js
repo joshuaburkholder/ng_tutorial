d3b642ec0a2f33a823bd3f57fa356ee95b328635
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-10-04 23:28:31 -0400

    update class

--- typescript_tut/ts-hello/main.js
+++ typescript_tut/ts-hello/main.js
@@ -1,39 +1,11 @@
-//Typescript will set assumed time, 
-//throwing a compiling error
-var count = 5;
-count = 'a';
-//Declaring a var with let without assigning any value,
-// will set the type to 'any'
-var a;
-a = 1;
-// a = true;
-// a = 'word';
-//If you do not know the value of a variable, 
-//you can still set the type
-var b;
-b = 1;
-// b = true;
-// b = 'word';
-//Variable types:
-var ab;
-var cd;
-var ef;
-var gh;
-var ij = [1, 2, 3];
-var kl = [1, true, 'string', false];
-var colorRed = 0;
-var colorGreen = 1;
-var colorBlue = 2;
-//enumerating containers; will be assigned(zero-based)
-//example:
-// enum Color {Red, Green, Blue };
-// let backgroundColor = Color.Blue;
-//Best to assign values
-var Color;
-(function (Color) {
-    Color[Color["Red"] = 0] = "Red";
-    Color[Color["Green"] = 1] = "Green";
-    Color[Color["Blue"] = 2] = "Blue";
-})(Color || (Color = {}));
-;
-var backgroundColor = Color.Blue;
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+        console.log(this.x, this.y);
+    };
+    Point.prototype.getDistance = function () {
+        console.log(this.x - this.y);
+    };
+    return Point;
+}());
--- typescript_tut/ts-hello/main.ts
+++ typescript_tut/ts-hello/main.ts
@@ -1,59 +1,14 @@
 
-//Typescript will set assumed time, 
-//throwing a compiling error
-let count = 5;
-count = 'a';
 
-//Declaring a var with let without assigning any value,
-// will set the type to 'any'
-let a;
-a = 1;
-// a = true;
-// a = 'word';
-
-//If you do not know the value of a variable, 
-//you can still set the type
-let b:number;
-
-b = 1;
-// b = true;
-// b = 'word';
-
-//Variable types:
-let ab: number;
-let cd: boolean;
-let ef: string;
-let gh: any;
-let ij: number[] = [1, 2, 3];
-let kl: any[] = [1, true, 'string', false];
-
-const colorRed = 0;
-const colorGreen = 1;
-const colorBlue = 2;
-
-//enumerating containers; will be assigned(zero-based)
-//example:
-   // enum Color {Red, Green, Blue };
-   // let backgroundColor = Color.Blue;
-
-//Best to assign values
-enum Color {Red = 0, Green = 1, Blue = 2 };
-let backgroundColor = Color.Blue;
-
-
-//TYPE ASSERTIONS
-
-let message;
-message = 'abc';
-
-let endsWithC = (<string>message).endsWith('c');
-let endsWithC2 = (message as string).endsWith('c');
-
-
-let log = function(message){
-    console.log(message);
+class Point {
+    x: number;
+    y: number;
+    draw() {
+        console.log(this.x,this.y);
+    }
+    
+    getDistance(){
+       console.log(this.x - this.y);
+    }
 }
 
-let doLog = (message) => {
-    console.log(message);
-}

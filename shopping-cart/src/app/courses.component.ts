3506060bc0a9a26e49f7b8029364f18f4a3f6ffc
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-10-07 02:03:03 -0400

    init service

--- shopping-cart/src/app/courses.component.ts
+++ shopping-cart/src/app/courses.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ getTitle() }}</h2>'
+    template: `
+        <h2>{{ title() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+            {{ course }}
+            </li>
+        </ul>
+        `
 })
 export class CoursesComponent {
     title = "List of courses";
+    courses = ["First Course", "Second Course", "Third Course"];
+
+    //Logic for calling an HTTP service
     
-    getTitle(){
-        return this.title;
-    }
 }
